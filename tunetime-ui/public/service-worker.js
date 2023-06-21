9d5e45dbd95e8dae9d8af8c981f8cce2384120f4
Sam Wolfson <terabyte128@gmail.com>
2023-06-20 21:43:16 -0700

    worker

--- tunetime-ui/public/service-worker.js
+++
@@ -1,6 +0,0 @@
-/// <reference lib="webworker" />
-self.addEventListener('push', (event) => {
-    if (event.data) {
-        self.registration.showNotification(event.data.text());
-    }
-});
--- tunetime-ui/worker/service-worker.ts
+++ tunetime-ui/worker/service-worker.ts
@@ -1,7 +1,11 @@
 /// <reference lib="webworker" />
 
+declare const self: ServiceWorkerGlobalScope;
+
 self.addEventListener('push', (event: PushEvent) => {
     if (event.data) {
         self.registration.showNotification(event.data.text());
     }
 });
+
+export default null;
