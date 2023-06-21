5432ea8b82fa0172ba903db070bbd0229974b466
Sam Wolfson <terabyte128@gmail.com>
2023-06-21 00:28:22 -0700

    make pretty

--- tunetime-ui/src/App.tsx
+++ tunetime-ui/src/App.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import { DateTime } from 'luxon';
 import { components } from './spotify';
 import './App.css';
 import useSWR, { SWRConfig } from 'swr';
@@ -42,6 +43,7 @@ const useRecent = () =>
 const HelloContainer = () => {
     const profile = useProfile();
     const recent = useRecent();
+    const { mutate } = useTunes();
 
     if (profile.error) {
         window.location.href = '/oauth2/login';
@@ -54,6 +56,7 @@ const HelloContainer = () => {
     return (
         <div>
             <div>
+                <PushNotifications />
                 <p>you are logged in as {profile.data.display_name}</p>
                 <p>
                     and your most recently played track was{' '}
@@ -62,19 +65,64 @@ const HelloContainer = () => {
                     the album <b>{recent.data.album?.name}</b>.
                 </p>
                 <button
-                    onClick={() => {
-                        fetch('/api/profile/share-tune', {
+                    style={{ width: '100%', height: '60px', fontSize: '18px' }}
+                    onClick={async () => {
+                        await fetch('/api/profile/share-tune', {
                             method: 'POST',
                         });
+                        mutate();
                     }}
                 >
                     tell your friends!
                 </button>
             </div>
-            <div style={{ marginTop: '24px' }}>
-                <PushNotifications />
-            </div>
+            <TuneCards />
         </div>
+    );
+};
+
+interface Tune {
+    name: string;
+    album: string;
+    primary_artist: string;
+    user: string;
+    created_at: number;
+}
+
+const useTunes = () => useSWR<Tune[]>('/api/tunes');
+
+const TuneCards = () => {
+    const { data, error } = useTunes();
+
+    if (!data || error) {
+        return <>loading tunes...</>;
+    }
+
+    return (
+        <>
+            <h3>your friends have been listening to...</h3>
+            {data.map(tune => (
+                <div className="card">
+                    <div className="flex-horizontal space-between">
+                        <div>
+                            {tune.name}
+                            <br />
+                            {tune.primary_artist}
+                        </div>
+                        <div>{tune.album}</div>
+                    </div>
+                    <hr />
+                    <div className="flex-horizontal space-between">
+                        <div>{tune.user}</div>
+                        <div>
+                            {DateTime.fromSeconds(
+                                tune.created_at,
+                            ).toLocaleString(DateTime.DATETIME_MED)}
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </>
     );
 };
 
--- tunetime/app.py
+++ tunetime/app.py
@@ -1,14 +1,112 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.engine import create
+
+from tunetime.db import make_session
+from tunetime.gchat import send_chat_message
+from tunetime.models import Tune, User
+from tunetime.oauth import SessionDependency
+from tunetime.spotify.types import PrivateUserObject, TrackObject
 
 from . import oauth
-from .api import user
 
 app = FastAPI()
 
 app.include_router(oauth.router, prefix="/oauth2")
-app.include_router(user.router, prefix="/api/profile")
 
 
 @app.get("/")
 def root():
     return "root"
+
+
+@app.get("/api/profile", response_model=PrivateUserObject)
+def get_user(session: SessionDependency):
+    return session["spotify_client"].get_profile()
+
+
+@app.get("/api/profile/recent", response_model=TrackObject)
+def get_recent(session: SessionDependency):
+    return session["spotify_client"].latest_track()
+
+
+class PushRegistration(BaseModel):
+    endpoint: str
+    keys: dict
+
+
+@app.post("/api/profile/register-push")
+def register_push(
+    session: SessionDependency, push_registration: PushRegistration
+):
+    with make_session() as db_session:
+        login_session = session["login_session"]
+        login_session.push_registration = push_registration.dict()
+
+        db_session.add(login_session)
+        db_session.commit()
+
+
+@app.delete("/api/profile/register-push")
+def delete_push(session: SessionDependency):
+    with make_session() as db_session:
+        login_session = session["login_session"]
+        login_session.push_registration = None
+
+        db_session.add(login_session)
+        db_session.commit()
+
+
+@app.post("/api/profile/share-tune")
+def share(session: SessionDependency):
+    spotify = session["spotify_client"]
+    login_session = session["login_session"]
+    user = login_session.user
+
+    latest_track = spotify.latest_track()
+    profile = spotify.get_profile()
+
+    with make_session() as db_session:
+        db_session.add(user)
+
+        user.tunes.append(
+            Tune(
+                spotify_id=latest_track.id,
+                name=latest_track.name,
+                album=latest_track.album.name,
+                primary_artist=latest_track.artists[0].name,
+            )
+        )
+
+        db_session.commit()
+
+    message = f"{profile.display_name} most recently listened to {latest_track.name} by {', '.join([i.name for i in latest_track.artists])} from the album {latest_track.album.name}: {latest_track.external_urls.spotify}"
+
+    send_chat_message(message)
+
+
+class TuneResponse(BaseModel):
+    name: str
+    album: str
+    primary_artist: str
+    user: str
+    created_at: int
+
+
+@app.get("/api/tunes", response_model=list[TuneResponse])
+def tunes():
+    with make_session() as db_session:
+        stmt = select(Tune).join(User).order_by(Tune.created_at.desc())
+        data = db_session.scalars(stmt).all()
+
+        return [
+            TuneResponse(
+                name=i.name,
+                album=i.album,
+                primary_artist=i.primary_artist,
+                user=i.user.spotify_id,
+                created_at=i.created_at,
+            )
+            for i in data
+        ]
